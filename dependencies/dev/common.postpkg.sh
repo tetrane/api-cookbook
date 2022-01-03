e858478f71543c1705c891885409166daefd464a
Louis Dureuil <ldureuil@tetrane.com>
2022-01-03 09:51:13 +0100

    GitLab: Install mdbook and mdbook-tera binaries instead of building them

--- dependencies/dev/common.postpkg.sh
+++ dependencies/dev/common.postpkg.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Install rust
-echo "Installing rust"
-wget -O - https://sh.rustup.rs | sh -s -- -y -q
-
-# Install rust dependencies
+# Install mdbook binaries
 echo "Installing mdbook"
-source $HOME/.cargo/env
-cargo install --version "0.4.13" mdbook
-cargo install --version "0.4.0" mdbook-tera
+wget -O - https://github.com/rust-lang/mdBook/releases/download/v0.4.14/mdbook-v0.4.14-x86_64-unknown-linux-gnu.tar.gz | tar xvzf -
+echo "Installing mdbook-tera"
+wget -O - https://github.com/avitex/mdbook-tera/releases/download/v0.4.2/mdbook-tera-x86_64-unknown-linux-gnu.tar.gz | tar xvzf -
+
