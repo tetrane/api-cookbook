86fe5cdef13d7a863cdea2cf00e42f5c284df338
Skia <fjacquet@tetrane.com>
2022-06-07 11:18:57 +0200

    Install tools in /usr/local/bin

--- dependencies/dev/common.postpkg.sh
+++ dependencies/dev/common.postpkg.sh
@@ -2,7 +2,6 @@
 
 # Install mdbook binaries
 echo "Installing mdbook"
-wget -O - https://github.com/rust-lang/mdBook/releases/download/v0.4.14/mdbook-v0.4.14-x86_64-unknown-linux-gnu.tar.gz | tar xvzf -
+wget -O - https://github.com/rust-lang/mdBook/releases/download/v0.4.14/mdbook-v0.4.14-x86_64-unknown-linux-gnu.tar.gz | tar -xvzf - -C /usr/local/bin
 echo "Installing mdbook-tera"
-wget -O - https://github.com/avitex/mdbook-tera/releases/download/v0.4.2/mdbook-tera-x86_64-unknown-linux-gnu.tar.gz | tar xvzf -
-
+wget -O - https://github.com/avitex/mdbook-tera/releases/download/v0.4.2/mdbook-tera-x86_64-unknown-linux-gnu.tar.gz | tar -xvzf - -C /usr/local/bin
